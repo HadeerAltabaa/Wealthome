cfa48bcde3d412692e3bb72322a02bf152fd8cfc
Hadeer.Altabaa <hadeer.altabaa89@gmail.com>
2026-04-14 10:30:42 +0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/**
+ * navbar toggle in mobile
+ */
+
+const /** {NodeElemeent} */ $navbar = document.querySelector("[data-navbar]");
+const /** {NodeElemeent} */ $navToggler = document.querySelector("[data-nav-toggler]");
+
+$navToggler.addEventListener("click", () => $navbar.classList.toggle("active"))
+
+/**
+ * Header scroll state
+ */
+
+const /** {NodeElemeent} */ $header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", e => {
+    $header.classList[window.scrollY > 50 ? "add" : "remove"]("active");
+});
+
+/**
+ * Add to favorite button toggle
+ */
+const /** {NodeList} */ $toggleBtns = document.querySelectorAll("[data-toggle-btn]");
+
+$toggleBtns.forEach($toggleBtn => {
+    $toggleBtn.addEventListener("click", () => {
+        $toggleBtn.classList.toggle("active");
+    })
+})
